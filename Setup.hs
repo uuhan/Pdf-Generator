180c0a9d40c61a0c43d41028dfe9d3b06d7fa6f9
徐敏辉 <xuminhui@innobuddy.com>
2017-12-09 00:08:20 +0800

    add build dep

--- Setup.hs
+++ Setup.hs
@@ -1,5 +1,6 @@
 import           Control.Monad
-import           Distribution.PackageDescription    (PackageDescription (..))
+import           Data.Maybe                         (fromJust)
+import           Distribution.PackageDescription
 import           Distribution.Simple
 import           Distribution.Simple.LocalBuildInfo (InstallDirs (..),
                                                      LocalBuildInfo (..),
@@ -12,7 +13,10 @@ import           System.Directory
 main :: IO ()
 main = defaultMainWithHooks simpleUserHooks
   { preConf = \a b -> makeLib a b >> preConf simpleUserHooks a b
-  , copyHook = copyLibharu
+  , confHook = \a f -> confHook simpleUserHooks a f >>= updateExtraLibDirs
+  , postConf = \_ _ _ _ -> return ()
+  , preBuild = updateLibDirs
+  , postCopy = copyLibharu
   }
 
 makeLib :: Args -> ConfigFlags -> IO ()
@@ -24,6 +28,34 @@ makeLib _ flags = do
     , ["make"]
     ]
 
+updateExtraLibDirs :: LocalBuildInfo -> IO LocalBuildInfo
+updateExtraLibDirs lbi = do
+  let pkg_descr = localPkgDescr lbi
+      lib = fromJust $ library pkg_descr
+      libBuild = libBuildInfo lib
+      libPref = libdir $ absoluteInstallDirs pkg_descr lbi NoCopyDest
+  return lbi { localPkgDescr = pkg_descr {
+                 library = Just $ lib { libBuildInfo = libBuild {
+                                          extraLibDirs = libPref : extraLibDirs libBuild
+                                        }
+                                  }
+               }
+         }
+
+updateLibDirs :: Args -> BuildFlags -> IO HookedBuildInfo
+updateLibDirs _ _ = do
+  dir <- getCurrentDirectory
+  let libharuDir = dir ++ "/libharu/src"
+      bi = emptyBuildInfo { extraLibDirs = [ libharuDir ] }
+  return (Just bi, [])
+
+copyLibharu :: Args -> CopyFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+copyLibharu _ flags desc lbi = do
+  let libPref = libdir . absoluteInstallDirs desc lbi . fromFlag . copyDest $ flags
+      v = fromFlag $ copyVerbosity flags
+   in runInDir v "libharu"
+        [["cp", "src/libhpdfs.a", libPref]]
+
 runInDir :: Verbosity -> FilePath -> [[String]] -> IO ()
 runInDir v path cmds = do
   old <- getCurrentDirectory
@@ -34,9 +66,3 @@ runInDir v path cmds = do
     run cmd = do
       rawSystemExit v "env" cmd
 
-copyLibharu :: PackageDescription -> LocalBuildInfo -> UserHooks -> CopyFlags -> IO ()
-copyLibharu desc lbi _ flags = do
-  let libPref = libdir . absoluteInstallDirs desc lbi . fromFlag . copyDest $ flags
-      v = fromFlag $ copyVerbosity flags
-  runInDir v "libharu"
-    [["cp", "src/libhpdfs.a", libPref]]
