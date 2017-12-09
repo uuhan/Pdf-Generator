5dd3482d5cd2a8621b156e6809d7bba2e6be8105
uuhan <xuminhui189@gmail.com>
2017-12-10 01:57:34 +0800

    use parsec for DSL to write & draw in pdf

--- Main.hs
+++ Main.hs
@@ -2,19 +2,21 @@ module Main where
 
 import           Foreign.Ptr
 import           Graphics.PDF.LibHaru
-import qualified Graphics.PDF.LibHaru.Page  as Page
+import qualified Graphics.PDF.LibHaru.Page     as Page
 import           Graphics.PDF.LibHaru.Types
 import           Graphics.PDF.LibHaru.Utils
 
+import           Text.ParserCombinators.Parsec
+
+data Write = WriteT Position String
+           | WriteP Position FilePath
+
 main :: IO ()
 main = do
-    doc <- new handler
+    doc <- new error_handler
     page <- addPage doc
     font <- getFont doc "Helvetica" nullPtr
     Page.setWidth page 220
     Page.setHeight page 200
     saveToFile doc "test.pdf"
     return ()
-
-foreign import ccall unsafe "utils.h &error_handler"
-  handler :: ErrorHandler
