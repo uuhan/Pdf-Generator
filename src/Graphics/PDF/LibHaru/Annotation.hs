8876535b1858359c3f02b47ba9768965a923551a
uuhan <xuminhui189@gmail.com>
2017-12-10 18:01:43 +0800

    add PopupAnnot

--- src/Graphics/PDF/LibHaru/Annotation.hs
+++ src/Graphics/PDF/LibHaru/Annotation.hs
@@ -5,4 +5,5 @@ module Graphics.PDF.LibHaru.Annotation
 import           Graphics.PDF.LibHaru.Annotation.Annot       as Annot
 import           Graphics.PDF.LibHaru.Annotation.LinkAnnot   as Annot
 import           Graphics.PDF.LibHaru.Annotation.MarkupAnnot as Annot
+import           Graphics.PDF.LibHaru.Annotation.PopupAnnot  as Annot
 import           Graphics.PDF.LibHaru.Annotation.TextAnnot   as Annot
---
+++ src/Graphics/PDF/LibHaru/Annotation/PopupAnnot.chs
@@ -0,0 +1,16 @@
+module Graphics.PDF.LibHaru.Annotation.PopupAnnot where
+
+import Foreign.Ptr
+import Foreign.C.Types
+import Foreign.C.String
+import Foreign.Storable
+import Foreign.Marshal.Alloc
+
+import Data.Word (Word64)
+
+{#import Graphics.PDF.LibHaru.Types#}
+{#import Graphics.PDF.LibHaru.Objects#}
+
+#include "hpdf.h"
+
+{#context prefix = "HPDF_PopupAnnot"#}
