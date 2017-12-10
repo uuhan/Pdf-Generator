65818cc37961bddb953b02967d3d1750fc56f180
uuhan <xuminhui189@gmail.com>
2017-12-10 18:06:09 +0800

    add FreeTextAnnot

--- src/Graphics/PDF/LibHaru/Annotation.hs
+++ src/Graphics/PDF/LibHaru/Annotation.hs
@@ -2,8 +2,9 @@ module Graphics.PDF.LibHaru.Annotation
     (module Annot)
   where
 
-import           Graphics.PDF.LibHaru.Annotation.Annot       as Annot
-import           Graphics.PDF.LibHaru.Annotation.LinkAnnot   as Annot
-import           Graphics.PDF.LibHaru.Annotation.MarkupAnnot as Annot
-import           Graphics.PDF.LibHaru.Annotation.PopupAnnot  as Annot
-import           Graphics.PDF.LibHaru.Annotation.TextAnnot   as Annot
+import           Graphics.PDF.LibHaru.Annotation.Annot         as Annot
+import           Graphics.PDF.LibHaru.Annotation.FreeTextAnnot as Annot
+import           Graphics.PDF.LibHaru.Annotation.LinkAnnot     as Annot
+import           Graphics.PDF.LibHaru.Annotation.MarkupAnnot   as Annot
+import           Graphics.PDF.LibHaru.Annotation.PopupAnnot    as Annot
+import           Graphics.PDF.LibHaru.Annotation.TextAnnot     as Annot
